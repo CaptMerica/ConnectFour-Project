9dac9dbbbe7ea684352c7f56fcb30726d8c115bb
CaptMerica <rmerica@live.com>
2022-12-19 12:02:40 -0500

    Add Variables, Caches, and Event Listeners

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ let board, turn, win, tie
 
 //---------------Cached Element References-------------//
 
-
+const circleEls = document.querySelectorAll(".cir")
+const messageEl = document.getElementById("message")
+const resetBtnlEl = document.getElementById("reset")
 
 //------------------Event Listeners-------------------//
 
-
-
+circleEls.forEach(circle =>circle.addEventListener("click", handleclick))
+resetBtnlEl.addEventListener("click", init)
 
 //---------------------Functions----------------------//
